fb2d8f42ba8b484fbf9a967691a234df79e1724f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 11:00:23 +0300

    Made some changes to the Submit component

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,15 +1,19 @@
 'use client'
-import React from 'react'
+
+import { useFormStatus } from 'react-dom'
 
 type Props = {}
 
 const SubmitButton = (props: Props) => {
+	const { pending } = useFormStatus()
+
 	return (
 		<button
 			type='submit'
+			aria-disabled={pending}
 			className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
 		>
-			Add Grudge
+			{pending ? 'Adding Grudge' : 'Add Grudge'}
 		</button>
 	)
 }
