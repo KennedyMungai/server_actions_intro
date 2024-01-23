912b93555613dc994acffc04d65ad55fec1aa739
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 09:18:32 +0300

    Delete some semicolons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Server Components Intro",
   description: "A simple project to introduce server components",
-};
+}
 
 export default function RootLayout({
   children,
@@ -18,5 +18,5 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
